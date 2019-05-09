1a889d500325f958e4ac01ced0fe43d0bfac6cc7
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-09 11:23:31 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/sqlite/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/sqlite/MainActivity.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     SQLite sqLite;
     ListView listView;
+    MonanAdapter monanAdapter;
+    ArrayList<Monan> mangmonan = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         // Tao database
         sqLite = new SQLite(this,"Quanlymonan.sql", null , 1);
         listView = findViewById(R.id.listview);
+        monanAdapter = new MonanAdapter(this,mangmonan);
+        listView.setAdapter(monanAdapter);
         // Tao bang
         //Cau truc du lieu
 //        String createtable = "CREATE TABLE IF NOT EXISTS Monan(Id INTEGER AUTO_INCREMENT PRIMARY KEY , Tenmonan VARCHAR , Diachi VARCHAR , Gia INTEGER)";
@@ -25,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         //Them du lieu vao trong bang
 //        String insertdata = "INSERT INTO Monan VALUES (null,'Bánh canh','Quận 3',30000)";
 //        sqLite.Querydata(insertdata);
+        selectDatabase();
+    }
+    private void selectDatabase(){
         String getAlldata = "SELECT * FROM Monan";
         Cursor cursor = sqLite.Getdata(getAlldata);
         while (cursor.moveToNext()){
@@ -32,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
             String ten = cursor.getString(1);
             String diachia = cursor.getString(2);
             int gia = cursor.getInt(3);
-
+            mangmonan.add(new Monan(id , ten ,diachia, gia));
         }
+        monanAdapter.notifyDataSetChanged();
     }
 }
